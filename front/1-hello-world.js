7f2ea5afca6615e7b72ce587d6a2d4400f020efd
Mahefa <abelmahefa@gmail.com>
2022-06-04 11:07:58 +0300

    Update

--- front/1-hello-world.js
+++ front/1-hello-world.js
@@ -1,3 +1,12 @@
 console.log("Hello, world");
 console.warn("Alert, world");
-console.error("World, climat harm")
+console.error("World, climat harm");
+
+const pi = 3.141592;
+let nom = "Koto"
+const c = "initial value";
+let isChecked = false;
+let personne = null;
+let voiture;
+
+console.log(pi, personne, voiture)
